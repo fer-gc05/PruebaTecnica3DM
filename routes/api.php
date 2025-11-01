ceac480f4e5bda415f26acd7c813eb2f4b12479b
Fernando <gilcorrea09@gmail.com>
2025-10-31 22:32:52 -0500

    :truck: Definir rutas API y web (routes/api.php, routes/web.php, routes/console.php)

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ResultController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Ruta para realizar la carga inicial de datos.
+Route::post('/perform-init-load', [ApiController::class, 'performInitLoad']);
+
+// Ruta para realizar el barrido de datos.
+Route::post('/perform-sweep', [ApiController::class, 'performSweep']);
+
+// Ruta para generar el reporte final con métricas.
+Route::post('/generate-report', [ApiController::class, 'generateReport']);
+
+// Ruta para gestionar los resultados.(CRUD)
+Route::apiResource('results', ResultController::class);
